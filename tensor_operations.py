19c6c64c26b82abf8ceac9eb4bf5262f9e499ccf
gh <jamespolkuudery@gmail.com>
2024-12-27 20:42:57 +0800

    subtraction

--- tensor_operations.py
+++ tensor_operations.py
@@ -16,3 +16,12 @@ print(f"Torch method addition = {addition}")
 # Inplace addition
 y.add_(x) # any function with a trailing underscore name_ will do an in place operation
 print(f"In-place addition {y}")
+
+#Subtraction
+subtractionOne = x - y
+print(f"\n\nSUBTRACTION\nFirst Subtraction = {subtractionOne}")
+# or
+subtraction = torch.subtract(x,y)
+print(f"Torch method subtraction = {subtraction}")
+x.sub_(y)
+print(f"In-place subtraction {x}")
