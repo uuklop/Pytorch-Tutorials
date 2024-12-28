7a5a73d8e5c12f81e78fbfff401af0b06972e2bf
gh <jamespolkuudery@gmail.com>
2024-12-28 18:52:26 +0800

    Prevent_tracking_history

--- autograd.py
+++ autograd.py
@@ -27,3 +27,16 @@ print(tryTwo)
 #Fixing it
 fixing_it = torch.tensor([0.1,1.0,0.001], dtype=torch.float32)
 tryTwo.backward(fixing_it)
+
+#Preventing Pytorch from tracking history
+
+# 1. Call the x.requires_grad_(False)
+x.requires_grad_(False)
+print(x)
+# 2. x.detach()
+y = x.detach()
+print(y)
+# 3. Wrap in a with statement with.no_grad()
+with torch.no_grad():
+    y = x + 2
+    print(y)
